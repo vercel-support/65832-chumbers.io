14f02b7673be9be14f997a79c4b060a468d379cb
nickmarks00 <nickmarks00@gmail.com>
2021-11-24 18:29:24 +1100

    Base plate code with all priority dependencies

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import "tailwindcss/tailwind.css"
+import "../../styles/globals.scss"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1 className="text-red-600">Hello there!</h1>
+    </div>
+  )
 }
